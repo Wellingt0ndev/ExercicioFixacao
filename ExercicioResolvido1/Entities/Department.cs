b36013d1ae54dd9879360cbfb684db35a6e12abb
Wellington Ferreira da Silva <108769429+Wellingt0ndev@users.noreply.github.com>
2024-01-18 15:46:30 -0300

    Exercicio Resolvido

---
+++ ExercicioResolvido1/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ExercicioResolvido1.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
